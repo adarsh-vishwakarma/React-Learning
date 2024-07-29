d0923b88ba4e4fb30aede6678499dd6e4a294f89
Adarsh Vishwakarma <163298836+adarsh-vishwakarma@users.noreply.github.com>
2024-07-29 09:15:58 +0530

    Add files via upload

---
+++ React-Part-6/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full h-96 relative flex items-center justify-center'>
+        <h1 className='text-6xl font-semibold bg-orange-500 text-white rounded-md py-2 px-5 text-center'>HOME</h1>
+    </div>
+  )
+}
+
+export default Home
