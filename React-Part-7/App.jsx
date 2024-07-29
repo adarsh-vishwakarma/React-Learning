af81c636fd26c994f339a9e73abd28095855e1a0
Adarsh Vishwakarma <163298836+adarsh-vishwakarma@users.noreply.github.com>
2024-07-29 10:00:18 +0530

    Create App.jsx

---
+++ React-Part-7/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Routing from './Utils/Routing';
+
+
+const App = () => {
+  return (
+    <>
+      
+      <Navbar />
+      <Routing />
+      
+    </>
+  )
+}
+
+export default App;
